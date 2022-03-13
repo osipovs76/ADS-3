d6559a21c054cfdabd12813598cb7bea4a02b48a
Osipov Andrey(21-IST5) <94702769+osipovs76@users.noreply.github.com>
2022-03-13 13:46:57 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,8 +3,8 @@
 int cbinsearch(int *arr, int size, int value) {
   int c = 0;
   for (int i = 0; i < size; i++) 
-		  if (arr[i] == value) 
-			  c++;
-		return c;
+     if (arr[i] == value) 
+	c++;
+   return c;
   return 0; // если ничего не найдено
 }
